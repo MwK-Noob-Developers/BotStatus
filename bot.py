dd8367ead5aa107e8f2c642dc23ae7e21523a61c
Shamil Habeeb <shamilhabeeb5@gmail.com>
2021-08-01 10:37:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,9 +51,9 @@ async def BotzHub():
                 msg = history.messages[0].id
                 if snt.id == msg:
                     print(f"@{bot} is down.")
-                    edit_text += f"🤖 @{bot}\n📊 Status: `DOWN` ❌\n"
+                    edit_text += f"**🤖 {bot}** \n📊 Status: `DOWN` ❌\n\n"
                 elif snt.id + 1 == msg:
-                    edit_text += f"🤖 @{bot} \n📊 Status: `UP` ✅\n"
+                    edit_text += f"**🤖 {bot}** \n📊 Status: `UP` ✅\n\n"
                 await user_bot.send_read_acknowledge(bot)
                 c += 1
                 await user_bot.edit_message(int(chnl_id), msg_id, edit_text)
